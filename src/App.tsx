bd4edb97b1a87293408ecede2f669fc34e06af6f
Front-Sanghoo <lettucegood2@gmail.com>
2026-08-26 00:31:50 +0900

    chore: 팀 이름 입력 단계를 제거하고 대시보드로 통합

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
 import { Outlet } from 'react-router'
-import { TeamNameGate } from './features/team/components/TeamNameGate.tsx'
-import type { WorkspaceContext } from './routes/workspaceContext.ts'
 
 function App() {
-  const [teamName, setTeamName] = useState<string | null>(null)
-
-  if (!teamName) {
-    return <TeamNameGate onContinue={setTeamName} />
-  }
-
-  return <Outlet context={{ teamName } satisfies WorkspaceContext} />
+  return <Outlet />
 }
 
 export default App
--- src/routes/workspaceContext.ts
+++
@@ -1,3 +0,0 @@
-export interface WorkspaceContext {
-  teamName: string
-}
