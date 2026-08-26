0e68e76654af04bfa616b16df001ffb03fce3497
Front-Sanghoo <lettucegood2@gmail.com>
2026-08-26 09:43:53 +0900

    chore: baseurl 변경

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://api.mvpilot.cloud:8080",
+  baseURL: "http://api.mvpilot.cloud:8080",
   headers: {
     Accept: "application/json",
   },
