85a0ce9dbb77f909fb8803b7e38e64d4790fd084
Front-Sanghoo <lettucegood2@gmail.com>
2026-08-26 13:05:11 +0900

    test

--- src/features/project/hooks/useAnalyzePdf.ts
+++ src/features/project/hooks/useAnalyzePdf.ts
@@ -26,7 +26,7 @@ async function createProjectFromPdf({
   const projectResponse = await api.post<ApiResponse<ProjectResponse>>(
     "/api/v1/projects",
     {
-      sourcePdfId: 25,
+      sourcePdfId: 29,
       name,
     },
   );
